c470c005394d7f453595296e75a8b769612a658b
Amrit <stanamrit@gmail.com>
2024-01-24 16:41:22 +0545

    typos fixed

--- utils/database.js
+++ utils/database.js
@@ -4,13 +4,13 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
-
+  const mongodbUri = process.env.MONGODB_URI;
   if (isConnected) {
     console.log("Mongodb is already connected");
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(mongodbUri, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
