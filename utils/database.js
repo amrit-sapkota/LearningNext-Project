f221446710d1d3b22de649b70a657f5313a0c0a3
Amrit <stanamrit@gmail.com>
2024-01-25 10:35:37 +0545

    fix

--- utils/database.js
+++ utils/database.js
@@ -6,17 +6,20 @@ export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (isConnected) {
-    console.log("Mongodb is already connected");
+    console.log("MongoDB is already connected");
     return;
   }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
-    console.log("Mongodb is connected");
+
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
